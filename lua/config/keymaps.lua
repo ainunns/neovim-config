7596f6ddfc049242259fc0204b0ecfca187486db
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2025-09-09 22:52:50 +0700

    feat: add clipboard config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Copy/paste conveniences
+-- Yank to system clipboard with <leader>y and <leader>Y
+map({ "n", "v" }, "<leader>y", "+y", { desc = "Yank to system clipboard" })
+map("n", "<leader>Y", "+Y", { desc = "Yank line to system clipboard" })
+
+-- Paste from system clipboard with <leader>p and <leader>P
+map({ "n", "v" }, "<leader>p", "+p", { desc = "Paste from system clipboard" })
+map({ "n", "v" }, "<leader>P", "+P", { desc = "Paste before from system clipboard" })
+
+-- Keep default p behavior from black-holing when pasting over selection
+map("x", "p", '"_dP', { desc = "Paste without yanking selection" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,42 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+
+-- Use the system clipboard for all yanks/deletes/puts
+-- Works on Linux, macOS, and (via provider below) WSL
+vim.opt.clipboard = "unnamedplus"
+
+-- Better default when pasting/code editing: do not continue comments automatically
+vim.opt.formatoptions:remove({ "c", "r", "o" })
+
+-- WSL clipboard provider: use win32yank if available; fallback to clip.exe + powershell
+if vim.fn.has("wsl") == 1 then
+  if vim.fn.executable("win32yank.exe") == 1 then
+    vim.g.clipboard = {
+      name = "win32yank-wsl",
+      copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
+      },
+      paste = {
+        ["+"] = "win32yank.exe -o --lf",
+        ["*"] = "win32yank.exe -o --lf",
+      },
+      cache_enabled = 0,
+    }
+  else
+    -- Fallback works if clip.exe and powershell.exe are on PATH (typical in WSL)
+    vim.g.clipboard = {
+      name = "wsl-clip",
+      copy = {
+        ["+"] = "clip.exe",
+        ["*"] = "clip.exe",
+      },
+      paste = {
+        ["+"] = 'powershell.exe -NoProfile -Command Get-Clipboard | tr -d "\\r"',
+        ["*"] = 'powershell.exe -NoProfile -Command Get-Clipboard | tr -d "\\r"',
+      },
+      cache_enabled = 0,
+    }
+  end
+end
